5c318e612d598632da48d49b1b85e5bfdd79c94b
miyuki harada <mykk.223@gmail.com>
2015-11-17 17:22:38 +0900

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("Hello");
+		System.out.println("Harada");
+  }
+}
