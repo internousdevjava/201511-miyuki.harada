8ac887e9c04d40dcf770f16673ea95218021f699
miyuki harada <mykk.223@gmail.com>
2015-11-20 15:14:16 +0900

    KisoKadai

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -32,7 +32,7 @@ public class KisoKadai1 {
 
 				if (isNumber(str)) {
 					x = Integer.parseInt(str);
-					if (x <= 100) {
+					if (x >= 1 && x <= 100) {
 						System.out.println("ｙの値を入れてね");
 						break;
 					} else {
@@ -47,55 +47,54 @@ public class KisoKadai1 {
 				e.printStackTrace();
 			}
 		}
-			while (flg) {
-				try {
-					BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-					str2 = br.readLine();
+		while (flg) {
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				str2 = br.readLine();
 
-					if (isNumber(str2)) {
-						y = Integer.parseInt(str2);
-						if (y <= 100) {
-							break;
-						} else {
-							System.out.println("入力値;1～100までの数値を入れてね");
-							continue;
-					}
-					}else {
-						System.out.println("入力値;数値を入れてね");
+				if (isNumber(str2)) {
+					y = Integer.parseInt(str2);
+					if (y >=1 && y <= 100) {
+						break;
+					} else {
+						System.out.println("入力値;1～100までの数値を入れてね");
 						continue;
 					}
-				}catch (Exception e) {
-
-					e.printStackTrace();
+				} else {
+					System.out.println("入力値;数値を入れてね");
+					continue;
 				}
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		x = Integer.parseInt(str);
+		y = Integer.parseInt(str2);
+
+		for (int i = 1; i <= x; i++) {
+			System.out.println();
+
+			for (int j = 1; j <= y; j++) {
+				if (i * j < 10) {
+					System.out.print("     ");
+				} else if (i * j < 100) {
+					System.out.print("    ");
+				} else if (i * j < 1000) {
+					System.out.print("   ");
+				} else if (i * j == 10000) {
+					System.out.print(" ");
+				} else {
+					System.out.print("  ");
+				}
+
+				System.out.print(" ");
+				System.out.print(i * j);
 			}
 
-				x = Integer.parseInt(str);
-				y = Integer.parseInt(str2);
-
-				for (int i = 1; i <= x; i++) {
-					System.out.println();
-
-					for (int j = 1; j <= y; j++) {
-					if (i * j < 10) {
-							System.out.print("     ");
-						} else if (i * j < 100) {
-							System.out.print("    ");
-						} else if (i * j < 1000) {
-							System.out.print("   ");
-						} else if (i * j == 10000) {
-							System.out.print(" ");
-						} else {
-							System.out.print("  ");
-						}
-
-						System.out.print(" ");
-						System.out.print(i * j);
-					}
-
-				}
-				}
-
+		}
+	}
 
 	public static boolean isNumber(String val) {
 		try {
