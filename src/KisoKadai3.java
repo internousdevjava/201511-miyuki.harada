d48580022f06dee008836c72d7aa2a89d44777a2
miyuki harada <mykk.223@gmail.com>
2015-11-24 13:26:12 +0900

    KisoKadai3

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -8,115 +8,103 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-/**
- *
- */
-
-/**
- * @author internous
- *
- */
 public class KisoKadai3 {
+	public static void main(String args[]) throws IOException {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws IOException {
-		System.out.println("--Hello--");
-
-		int end = 0;
-		while (end == 0) {
-
-			System.out.println("** メニュー **\n --1:読み書き用ファイルを指定する --0:終了--");
-			// フォルダ操作の場合
-			// 1.フォルダ作成
-			// 2.フォルダパスを表示する
-
-			// 処理開始
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String str = br.readLine();
-			int n = 0;
-			// エラー判定
-			if (str.matches("^[0-1]+$")) {
-				n = Integer.parseInt(str);
-			} else {
-				System.out.println("\n[1]か[0]でメニューを選択して下さい。\n");
-				continue;
-			}
-			// 終了判定
-			if (n == 0) {
-				System.out.println("終了します");
-				break;
-			}
-
-			boolean menu_flag = true;
-			while (menu_flag) {
+		boolean init = true;
+		while (init) {
+			// ファイルパスを指定する
+			File file =null;
+			// ディレクトリパスを取得する
+			File dir=null;
+			try{
 				// メニュー
 				// 1:読み書き用ファイルを指定する
 				System.out.println("\n読み書きする為のファイルを指定してください。");
 				System.out.println("例: C:\\Users\\internous\\test.txt\n");
 				System.out.println("終了する場合には99を入力してください。");
-				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-				String path = br2.readLine();
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String path = br.readLine();
+
+				if (path.length() == 0) {
+					System.err.println("何も入力されませんでした。\n再度入力してください。");
+					continue;
+				}
+
 				if(path.equals("99")){
-					System.out.println("終了します。");
+					System.out.println("処理を終了します。");
 					System.exit(0);
 				}
 
-				File directory=new File(path);
-				if(!directory.isFile()){
-					System.out.println("正しくありません。入力し直してください");
-					continue;
-				}
-
-				if(!directory.isDirectory()){
-					System.out.println("正しくありません");
-					continue;
-				}
-
 				// ファイルパスを指定する
-				File file = new File(path);
+				file = new File(path);
 				// ディレクトリパスを取得する
-				File dir = new File(file.getParent());
+				dir=new File(file.getParent());
 
-				if (!dir.exists()) {
+
+				if(!(file.isFile())){
+					System.err.println("ファイルが指定されていません");
+				}
+				if(!(file.isDirectory())){
+					System.err.println("ディレクトリが指定されていません");
+				}
+
+				if(!dir.exists()){
+					System.out.println("フォルダがありません。:"+file.getAbsolutePath());
 					dir.mkdirs();
-					System.out.println("指定されたフォルダを作成しました。");
+					System.out.println("作成成功");
 				} else {
-					System.out.println("指定されたフォルダは既に存在します。");
+					System.out.println("フォルダは既に存在します。");
 				}
-
-				if (file.exists()) {
-					System.out.println("指定されたファイルは既に存在します。\n" + file.getAbsolutePath());
-				} else {
-					System.out.println("ファイルは存在しません。\n新しく作成します。" + file.getAbsolutePath());
-					try {
-						if (file.createNewFile()) {
-							System.out.println("作成しました");
-						} else {
-							System.out.println("作成失敗しました");
-							System.out.println("再度指定してください");
-							continue;
-						}
-					} catch (IOException e) {
-						System.out.println(e);
+			}catch(Exception e){
+				System.err.println("入力に誤りがあります");
+				continue;
+			}
+			if(file.exists()){
+				System.out.println("ファイルは既に存在します。\n" + file.getAbsolutePath());
+			}else{
+				System.out.println("新しく作成します。\n"+file.getAbsolutePath());
+				try{
+					if (file.createNewFile()){
+						System.out.println("作成しました"+file.getAbsolutePath());
+					}else{
+						System.out.println("作成失敗しました");
+						System.out.println("再度指定してください");
 					}
+				}catch(IOException e){
+					System.out.println(e);
+				}
+			}
+
+
+
+			int end = 0;
+			while(end==0){
+
+				System.out.println("** メニュー **\n --1:書込み-- --2:読込み-- --0:終了-- ");
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+				String str = br2.readLine();
+				int nu = 0;
+
+				if(str.matches("^[0-9]+$")){
+					nu=Integer.parseInt(str);
+				}else{
+					System.out.println("\n----------\n半角数値でメニューを選択してください。\n----------\n");
 				}
 
+				if(nu==0){
+					System.out.println("終了します。");
+					break;
+				}
 
-				System.out.println("** メニュー **\n --1:書込み-- --2:読込み-- --9:終了-- ");
-				BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
-				String menu = br3.readLine();
-				if (menu.equals("1")) {
-					System.out.println("書込み処理を開始します");
+				if(nu==1){
 					try {
 						boolean mode = false;
 						System.out.println("書き込み方法を指定してください。\n1:追記、2:上書き");
-						BufferedReader br4 = new BufferedReader(new InputStreamReader(System.in));
-						String str4 = br4.readLine();
-
+						BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
+						String str2 = br3.readLine();
 						// モードを決める
-						switch (str4) {
+						switch (str2) {
 						case "1":
 							mode = true;
 							break;
@@ -125,14 +113,15 @@ public class KisoKadai3 {
 						default:
 							break;
 						}
+						System.out.println("書き込んでください");
 						//出力先を作成する
 						FileWriter fw = new FileWriter(file.getAbsolutePath(), mode);
 						PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-						System.out.println("書き込んでください");
+
 						//内容を指定する
-						BufferedReader br5 = new BufferedReader(new InputStreamReader(System.in));
-						String str5 = br5.readLine();
-						pw.println(str5);
+						BufferedReader br4 = new BufferedReader(new InputStreamReader(System.in));
+						String str3 = br4.readLine();
+						pw.println(str3);
 
 						//ファイルに書き出す
 						pw.close();
@@ -145,8 +134,8 @@ public class KisoKadai3 {
 						ex.printStackTrace();
 					}
 
-				}else if (menu.equals("2")) {
-					System.out.println("読込み処理を開始します");
+				}
+				if(nu==2){
 					try{
 						FileReader filereader = new FileReader(file.getAbsolutePath());
 
@@ -161,28 +150,14 @@ public class KisoKadai3 {
 					}catch(IOException e){
 						System.out.println(e);
 					}
-				}else if (menu.equals("9")) {
-					System.out.println("終了します。");
-					System.exit(0);
-				}else{
-					System.out.println("入力間違いがあります。再起動します。");
-					continue;
 				}
 			}
+			System.out.println("--処理終了--");
 		}
+
+
+		}
+
+
+
 	}
-}
-
-// ファイル読み取り
-
-/*
- * BufferedReader br2= new BufferedReader(new
- * java.io.InputStreamReader(System.in)); String str2 = br.readLine();
- *
- * try{ FileReader fil=new FileReader(file.getAbsolutePath());
- *
- * int aa; while((aa=fil.read())!=-1){ System.err.println((char)aa); }
- *
- * fil.close(); }catch(FileNotFoundException e){ System.out.println(e);
- * }catch(IOException e){ System.out.println(e); }
- */
